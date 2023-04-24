1e6580ca3d4f19fb5a379139a42b09e87c5d0731
AdityaS98 <adityasharmalko1998@gmail.com>
2023-04-25 02:34:56 +0530

    Added : A Programme for editing a contact in address Book

--- src/com/adressbooksystem/AddressBook.java
+++ src/com/adressbooksystem/AddressBook.java
@@ -77,49 +77,36 @@ public class AddressBook {
         System.out.println(list);
     }
 
+
     public static void main(String[] args) {
-        /*
-         * 1) Taking user input for contact details and set the details in contact class
-         * 2) Get the details form contact class and printing them
-         */
-        /*
-         1) Taking user input for contact details and set the details in contact class
-         */
 
-        ArrayList<Contact> list = new ArrayList<>();
+        AddressBook addressBook = new AddressBook();
         Scanner sc = new Scanner(System.in);
-        /*
-         * 1. Display Message
-         */
-        System.out.println("Please enter how much contact you want to add");
-        int noOfContact = sc.nextInt();
-        // Taking for loop for add multiple contact details
-        for (int i = 1; i <= noOfContact; i++) {
-            //Declaring Object
-            Contact contact = new Contact();
-            //Display messages for various parameter of the address book
-            System.out.println("Enter first name of contact ");
-            contact.setFirstName(sc.next());
-            System.out.println("Enter last name contact ");
-            contact.setLastName(sc.next());
-            System.out.println("Enter address contact ");
-            contact.setAddress(sc.next());
-            System.out.println("Enter city contact ");
-            contact.setCity(sc.next());
-            System.out.println("Enter state contact ");
-            contact.setState(sc.next());
-            System.out.println("Enter zip contact ");
-            contact.setZip(sc.nextInt());
-            System.out.println("Enter phone number  contact ");
-            contact.setPhoneNumber(sc.nextLong());
-            System.out.println("Enter email  contact ");
-            contact.setEmail(sc.next());
+        boolean b = true;
+        while (b) {
+            System.out.println("----Welcome to Address Book----");
+            System.out.println("Enter choice which you want to perform");
+            System.out.println("Enter 1 to add contact");
+            System.out.println("Enter 2 to edit contact");
+            System.out.println("Enter 3 to exit");
+            int button = sc.nextInt();
+            switch (button) {
+                case 1:
+                    addressBook.addContact();
+                    break;
+                case 2:
+                    addressBook.editContact();
+                    break;
+                default:
+                    b = false;
 
-            // Adding details in array list
-            list.add(contact);
+
+            }
         }
-        System.out.println(list);
-
-
     }
 }
+
+
+
+
+
